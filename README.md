24294f4be1bab950df629cf9cec35c5aa6d1efe8
Ian Yang <me@iany.me>
2013-02-27 19:26:51 +0800

    Better examples

--- README.md
+++ README.md
@@ -6,21 +6,21 @@ complete other hyphen separated choices
 
 ## Example
 
-Choices: "ido-foo-bar", "ido-space" "ido-test"
+`(ido-completing-read "test: " '("ido-foo-bar" "ido-space" "ido-test"))`
 
     | Key Sequence | Result |
     |--------------+--------|
     | i            | "i"    |
     | SPACE        | "ido-" |
 
-Choices: "ido-foo-bar", "ido-space" "ido-test"
+`(ido-completing-read "test: " '("ido foo-bar" "ido space" "ido test"))`
 
     | Key Sequence | Result |
     |--------------+--------|
     | i            | "i"    |
     | SPACE        | "ido " |
 
-Choices: "ido-foo-bar", "ido-space" "idotest"
+`(ido-completing-read "test: " '("ido-foo-bar" "ido-space" "idotest"))`
 
     | Key Sequence | Result |
     |--------------+--------|
@@ -30,15 +30,17 @@ Choices: "ido-foo-bar", "ido-space" "idotest"
 
 When HYPHEN can be inserted and SPACE cannot, insert HYPHEN when user enter SPACE.
 
-Choices: "ido-foo-bar", "ido-space" "ido test"
+`(ido-completing-read "test: " '("ido-foo-bar" "ido-space" "ido test"))`
 
-    | Key Sequence | Result |
-    |--------------+--------|
-    | i            | "i"    |
-    | SPACE        | "ido"  |
-    | SPACE        | "ido " |
+    | Key Sequence | Result                           |
+    |--------------+----------------------------------+
+    | i            | "i"                              |
+    | SPACE        | "ido"                            |
+    | SPACE        | "ido"  Completion popup is shown |
+    | SPACE        | "ido "                           |
 
-If bot HYPHEN and SPACE can be inserted, SPACE just inserts itself.
+If both HYPHEN and SPACE can be inserted, SPACE first brings the completion
+popup window, if user types SPACE again, then SPACE itself is inserted.
 
 ## Usage
 
--- ido-complete-space-or-hyphen.el
+++ ido-complete-space-or-hyphen.el
@@ -5,7 +5,7 @@
 ;; Filename: ido-complete-space-or-hyphen.el
 ;; Description: Complete SPACE or HYPHEN when type SPACE in ido
 ;; Created: 2012-11-07 13:58
-;; Version: 1.2
+;; Version: 1.1
 ;; Last-Updated: 2013-02-27 18:57
 ;; URL: https://github.com/doitian/ido-complete-space-or-hyphen
 
@@ -36,39 +36,41 @@
 ;;
 ;; Example:
 ;;
-;; Choices: "ido-foo-bar", "ido-space" "ido-test"
+;; (ido-completing-read "test: " '("ido-foo-bar" "ido-space" "ido-test"))
 ;;
 ;;     | Key Sequence | Result |
 ;;     |--------------+--------|
 ;;     | i            | "i"    |
 ;;     | SPACE        | "ido-" |
 ;;
-;; Choices: "ido-foo-bar", "ido-space" "ido-test"
-
+;; (ido-completing-read "test: " '("ido foo-bar" "ido space" "ido test"))
+;;
 ;;     | Key Sequence | Result |
 ;;     |--------------+--------|
 ;;     | i            | "i"    |
 ;;     | SPACE        | "ido " |
-
-;; Choices: "ido-foo-bar", "ido-space" "idotest"
-
+;;
+;; (ido-completing-read "test: " '("ido-foo-bar" "ido-space" "idotest"))
+;;
 ;;     | Key Sequence | Result |
 ;;     |--------------+--------|
 ;;     | i            | "i"    |
 ;;     | SPACE        | "ido"  |
 ;;     | SPACE        | "ido-" |
-
+;;
 ;; When HYPHEN can be inserted and SPACE cannot, insert HYPHEN when user enter SPACE.
-
-;; Choices: "ido-foo-bar", "ido-space" "ido test"
-
-;;     | Key Sequence | Result |
-;;     |--------------+--------|
-;;     | i            | "i"    |
-;;     | SPACE        | "ido"  |
-;;     | SPACE        | "ido " |
-
-;; If bot HYPHEN and SPACE can be inserted, SPACE just inserts itself.
+;;
+;; (ido-completing-read "test: " '("ido-foo-bar" "ido-space" "ido test"))
+;;
+;;     | Key Sequence | Result                           |
+;;     |--------------+----------------------------------+
+;;     | i            | "i"                              |
+;;     | SPACE        | "ido"                            |
+;;     | SPACE        | "ido"  Completion popup is shown |
+;;     | SPACE        | "ido "                           |
+;;
+;; If both HYPHEN and SPACE can be inserted, SPACE first brings the completion
+;; popup window, if user types SPACE again, then SPACE itself is inserted.
 
 ;;; Usage:
 
